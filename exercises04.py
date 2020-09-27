4c1d9c3c3cc061d4e552dbb54df4ac0177aafa36
Luiz <luiz.ado@hotmail.com>
2020-09-27 13:44:13 -0300

    Exercise 26 done.

--- exercises04.py
+++ exercises04.py
@@ -517,3 +517,47 @@ while(repeat == "S"):
   
   
 print()  # It will be used to print a blank line between de exercises.
+
+# 26 - Faça um programa que use a função valorPagamento para
+# determinar o valor a ser pago por uma prestação de uma conta
+# O programa deverá solicitar ao usuário o valor da prestação
+# e o número de dias em atraso e passar estes valores para a função
+# valorPagamento, que calculará o valor a ser pago e devolverá este
+# valor ao programa que a chamou. O programa deverá então exibir o
+# valor a ser pago na tela. Após a execução o programa deverá voltar
+# a pedir outro valor de prestação e assim continuar até que seja
+# informado um valor igual a zero para a prestação. Neste momento
+# o programa deverá ser encerrado, exibindo o relatório do dia, que
+# conterá a quantidade e o valor total de prestações pagas no dia.
+# O cálculo do valor a ser pago é feito da seguinte forma: Para
+# pagamentos sem atraso, cobrar o valor da prestação.Quando houver
+# atrasos, cobrar 3% de multa, mais 0,1% de juros por dia de atraso
+
+def valorPagamento(valorPrestacao, diasAtraso):
+  if(diasAtraso > 0):
+    novoValor = valorPrestacao + (valorPrestacao * 0.03) + (valorPrestacao * (diasAtraso * 0.001))
+    return novoValor
+  return valorPrestacao
+
+def paymentsReport():
+  payDayList = []
+  valorPrestacao = 0.01
+
+  while(valorPrestacao > 0):
+    valorPrestacao = float(input("Digite o valor da prestação: R$ "))
+    if(valorPrestacao == 0):
+      break
+    else:
+      diasAtraso = int(input("Digite a quantidade de dias em atraso: "))
+      amount = valorPagamento(valorPrestacao, diasAtraso)
+      payDayList.append(amount)
+  
+  payDayListTotal = sum(payDayList)
+
+  print(f"A quantidade total de prestações pagas foram: {len(payDayList)}")
+  print(f"O montante total pago foi de: R$ {round(payDayListTotal, 2)}")
+
+
+paymentsReport()
+
+print()  # It will be used to print a blank line between de exercises.
